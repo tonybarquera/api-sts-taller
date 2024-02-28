d45493f50ac8924461f820840e2d1c421ee94280
Tony Barquera <barqueratony@gmail.com>
2024-02-28 13:16:32 -0600

    sequelize connection

---
+++ database/database.js
@@ -0,0 +1,10 @@
+const { Sequelize } = require('sequelize');
+const { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER } = require('./../src/config.js');
+
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+  host: DB_HOST,
+  port: DB_PORT,
+  dialect: 'mysql'
+});
+
+module.exports = { sequelize };
--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { pool } = require('./db.js');
 const { PORT } = require('./config.js');
+const { sequelize } = require('./../database/database.js');
 
 const app = express();
 
@@ -8,9 +9,15 @@ app.get('/', (req, res) => {
   res.send("Welcome to server");
 });
 
-app.get('/ping', async (req, res) => {
-  const [ result ] = await pool.query(`SELECT "Hello World" as RESULT`);
-  res.send(result[0]);
+app.get('/test-connection', async (req, res) => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connected');
+  } catch(error) {
+    console.log('Error: ', error);
+  }
+
+  res.send("Hello World!");
 });
 
 app.listen(PORT, () => {
